d773c1333f275eadb520ea012e09a7de29576a94
Marcin Butora <36995841+marcinbutora@users.noreply.github.com>
2023-01-27 08:20:34 +0100

    Person repository

---
+++ src/main/java/com/vattenfall/libraryv2/repository/PersonRepository.java
@@ -0,0 +1,12 @@
+package com.vattenfall.libraryv2.repository;
+
+import com.vattenfall.libraryv2.entity.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface PersonRepository extends JpaRepository<Person, Long> {
+    Optional<Person> findPersonById(Long id);
+    Optional<Person> findPersonByFirstNameAndLastName(String firstName, String lastName);
+    Optional<Person> findPersonByPhoneNumber(String phoneNumber);
+}
