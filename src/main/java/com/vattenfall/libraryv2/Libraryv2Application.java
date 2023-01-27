b0e12d5c76ff0d247316fc3bdd5734522f9eaff1
Marcin Butora <36995841+marcinbutora@users.noreply.github.com>
2023-01-27 08:11:47 +0100

    initial commit

---
+++ src/main/java/com/vattenfall/libraryv2/Libraryv2Application.java
@@ -0,0 +1,13 @@
+package com.vattenfall.libraryv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Libraryv2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Libraryv2Application.class, args);
+    }
+
+}
